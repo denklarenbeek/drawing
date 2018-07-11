36e09448cb48fda76fa29296c321d72cbbe4963c
Dennis Klarenbeek <dennis.klarenbeek@icloud.com>
2018-07-11 16:59:12 +0200

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    DBUSER: 'admin',
-    DBPASS: 'admin12345',
-    MAIL_USER: 'be60f5b99badb1',
-    MAIL_PASS: '430836023651b5',
-    MAIL_HOST: 'smtp.mailtrap.io',
-    MAIL_PORT: 2525,
-};
