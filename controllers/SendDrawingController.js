73979cd9ebc395495a17b06d99ca10f6d3e43ad7
Dennis Klarenbeek <dennis.klarenbeek@icloud.com>
2018-07-13 13:38:11 +0200

    add cloud upload func

--- controllers/SendDrawingController.js
+++ controllers/SendDrawingController.js
@@ -5,6 +5,16 @@ const multer = require("multer");
 const path = require("path");
 const File = require("../models/Files");
 const mail = require("../handlers/mailer");
+const cloudinary = require('cloudinary');
+const fs = require('fs');
+const keys = require('../config/keys');
+
+//Set Cloudinary config
+cloudinary.config({ 
+    cloud_name: 'denklarenbeek', 
+    api_key: keys.CLOUD_KEY, 
+    api_secret: keys.CLOUD_SECRET
+});
 
 // Set storage engine
 const multerOptions = {
@@ -15,7 +25,7 @@ const multerOptions = {
 exports.upload =  multer(multerOptions).single("file");
 
 exports.saveActionToDB = async (req, res, next) => {
-
+    console.log(req.file.url);
     // Create a model to save in the database
     const fileUpload = new File({
         fromEmail: req.user.email,
@@ -54,4 +64,25 @@ exports.sendMail = async (req, res) => {
       ]
     });
     res.redirect('/');
+};
+
+exports.uploadToCloud = async (req, res, next) => {
+    await cloudinary.v2.uploader.upload_stream({resource_type: 'raw'}, {
+        eager: 
+        {
+            width: 150, height: 100, crop: "thumb", gravity: "face"
+        },
+            eager_async: true, 
+            eager_notification_url: "https://mysite/upload_success"
+        }, 
+        function(error, result){
+            if(error){
+                 console.log(error);
+                 next();
+            }
+            console.log(result.url);
+            req.file.url = result.url;
+        })
+        .end(req.file.buffer);
+    next();
 };
