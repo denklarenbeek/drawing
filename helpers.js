10066778c36f7d39fa0eb2ab66d5d6ae48ff2ac0
Dennis Klarenbeek <dennis.klarenbeek@icloud.com>
2018-07-13 19:39:22 +0200

    little bug fixes

--- helpers.js
+++ helpers.js
@@ -1,5 +1,5 @@
 exports.menu = [
     { slug: '/', title: 'home', icon: 'home', },
-    { slug: '/logout', title: 'logout', icon: 'lock', },
-    { slug: '/register', title: 'register', icon: 'user-plus', }
+    { slug: '/register', title: 'register', icon: 'user-plus', },
+    { slug: '/logout', title: 'logout', icon: 'lock', }
 ];
