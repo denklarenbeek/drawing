baa06446e93950f9adabb7869f15ab326f4d2979
Dennis Klarenbeek <dennis.klarenbeek@icloud.com>
2018-07-18 23:32:43 +0200

    add two keys in env

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -6,5 +6,7 @@ module.exports = {
     MAIL_HOST: process.env.MAIL_HOST,
     MAIL_PORT: process.env.MAIL_PORT,
     SECRET: process.env.SECRET,
-    KEY: process.env.KEY
+    KEY: process.env.KEY,
+    CLOUD_KEY: process.env.CLOUD_KEY,
+    CLOUD_SECRET: process.env.CLOUD_SECRET
 };
